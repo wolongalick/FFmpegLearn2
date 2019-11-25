35b3d940aa4d505e8e3c09c2ad1c42b19373a74b
cuixw <1607009565@qq.com>
2019-11-26 00:46:54 +0800

    将ffmpeg的库替换为3.4.6版本的

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,8 +3,8 @@
 #include <android/log.h>
 
 extern "C" {
-#include "include/libavcodec/avcodec.h"
-
+    #include <libavformat/avformat.h>
+    #include <libavcodec/avcodec.h>
 
 }
 
@@ -20,17 +20,59 @@ Java_com_alick_ffmpeglearn2_MainActivity_stringFromJNI(
     return env->NewStringUTF(hello.c_str());
 }
 
+
+char* jstringToChar(JNIEnv* env, jstring jstr) {
+    char* rtn = NULL;
+    jclass clsstring = env->FindClass("java/lang/String");
+    jstring strencode = env->NewStringUTF("GB2312");
+    jmethodID mid = env->GetMethodID(clsstring, "getBytes", "(Ljava/lang/String;)[B");
+    jbyteArray barr = (jbyteArray) env->CallObjectMethod(jstr, mid, strencode);
+    jsize alen = env->GetArrayLength(barr);
+    jbyte* ba = env->GetByteArrayElements(barr, JNI_FALSE);
+    if (alen > 0) {
+        rtn = (char*) malloc(alen + 1);
+        memcpy(rtn, ba, alen);
+        rtn[alen] = 0;
+    }
+    env->ReleaseByteArrayElements(barr, ba, 0);
+    return rtn;
+}
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_alick_ffmpeglearn2_MainActivity_parseFile(JNIEnv *env, jobject thiz, jstring file_path) {
 
-    std::string hello = "11111111111";
+    std::string hello = "配置";
+    hello+=avcodec_configuration();
 
-    LOGI("哈哈哈哈");
+    LOGI("ffmpeg配置:%s",hello.c_str());
 
-    avcodec_register_all();
+    //初始化解封装
+    av_register_all();
+    //初始化网路
+    avformat_network_init();
+
+    AVFormatContext *ic=NULL;
+    char* path=jstringToChar(env,file_path);
+    //打开媒体文件
+    int re = avformat_open_input(&ic, path, 0, 0);
+    if(re==0){
+        LOGI("打开文件成功,文件路径:%s ",path)
+    } else{
+        LOGE("打开文件失败:%s",av_err2str(re))
+    }
 
 
+//
+//    AVStream
+//    AVPacket
+
+//
+//    avformat_find_stream_info()
+//
+//    av_find_best_stream()
+//
+//    av_read_frame()
 
 
     LOGE("休息休息");
