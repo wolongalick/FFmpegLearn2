d6309f2f341b8410e6c03633db9ec66f3ee22b81
cuixw <1607009565@qq.com>
2019-12-03 00:08:08 +0800

    1.通过avcodec_find_decoder查找音视频解码器 2.通过avcodec_open2打开音视频解码器 3.通过av_read_frame读取帧数据

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -15,6 +15,7 @@ extern "C" {
 
 
 static double r2d(AVRational avRational) {
+    LOGI("num:%d,den:%d", avRational.num, avRational.den);
     return avRational.num == 0 || avRational.den == 0 ? 0 : (double) avRational.num /
                                                             (double) avRational.den;
 }
@@ -34,7 +35,12 @@ Java_com_alick_ffmpeglearn2_MainActivity_parseFile(JNIEnv *env, jobject thiz, js
 
     std::string hello = "11111111111";
 
+
     av_register_all();
+
+    avformat_network_init();
+
+    avcodec_register_all();
 
     std::string config = avcodec_configuration();
     LOGI("ffmpeg配置%s", config.c_str());
@@ -66,32 +72,101 @@ Java_com_alick_ffmpeglearn2_MainActivity_parseFile(JNIEnv *env, jobject thiz, js
     for (int i = 0; i < ic->nb_streams; ++i) {
         AVStream *pStream = ic->streams[i];
         if (pStream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
-            videoStream=i;
+            videoStream = i;
             LOGI("视频流索引:%d", i);
-            fps=r2d(pStream->avg_frame_rate);
-            LOGI("fps:%d,宽:%d,高:%d,解码器:%d,pixel_format:%d",fps,pStream->codecpar->width,pStream->codecpar->height,pStream->codecpar->codec_id,pStream->codecpar->format);
+            fps = r2d(pStream->avg_frame_rate);
+            LOGI("fps:%d,宽:%d,高:%d,解码器:%d,pixel_format:%d", fps, pStream->codecpar->width,
+                 pStream->codecpar->height, pStream->codecpar->codec_id, pStream->codecpar->format);
         } else if (pStream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-            audioStream=i;
+            audioStream = i;
             LOGI("音频流索引:%d", i);
-            LOGI("sample_rate:%d,channels:%d,sample_format:%d",pStream->codecpar->sample_rate,pStream->codecpar->channels,pStream->codecpar->format);
+            LOGI("sample_rate:%d,channels:%d,sample_format:%d", pStream->codecpar->sample_rate,
+                 pStream->codecpar->channels, pStream->codecpar->format);
         }
     }
 
     //
-    videoStream= av_find_best_stream(ic, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0);
+    videoStream = av_find_best_stream(ic, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0);
     LOGI("视频流索引:%d", videoStream);
-    audioStream= av_find_best_stream(ic, AVMEDIA_TYPE_AUDIO, -1, -1, nullptr, 0);
+    audioStream = av_find_best_stream(ic, AVMEDIA_TYPE_AUDIO, -1, -1, nullptr, 0);
     LOGI("音频流索引:%d", audioStream);
 
 
-//    av_q2d(ic->video_codec.);
+    /*==========================视频解码-begin=============================================================*/
+    //软解码器
+    AVCodec *pVideoAVCodec=avcodec_find_decoder(ic->streams[videoStream]->codecpar->codec_id);
+    //硬解码器
+//    pVideoAVCodec=avcodec_find_decoder_by_name("h254_mediacodec");
+
+    if(!pVideoAVCodec){
+        LOGE("查找视频解码器失败!");
+        return env->NewStringUTF("查找视频解码器失败!");
+    }
+
+    LOGI("查找视频解码器成功");
+
+    //视频解码器初始化
+    AVCodecContext *vc=avcodec_alloc_context3(pVideoAVCodec);
+    avcodec_parameters_to_context(vc,ic->streams[videoStream]->codecpar);
+    vc->thread_count=1;
+    //打开视频解码器
+    re= avcodec_open2(vc, nullptr, nullptr);
+    if(re!=0){
+        char *err = mergeStr(2, "打开视频解码器失败:", av_err2str(re));
+        LOGE("%s",err);
+        return env->NewStringUTF(err);
+    }
+
+    LOGI("打开视频解码器成功!");
+    /*===========================视频解码-end============================================================*/
+    /*===========================音频解码-begin============================================================*/
+
+    //软解码器
+    AVCodec *pAudioAVCodec=avcodec_find_decoder(ic->streams[audioStream]->codecpar->codec_id);
+    //硬解码器
+//    pVideoAVCodec=avcodec_find_decoder_by_name("h254_mediacodec");
+
+    if(!pAudioAVCodec){
+        LOGE("查找音频解码器失败!");
+        return env->NewStringUTF("查找音频解码器失败!");
+    }
+
+    LOGI("查找音频解码器成功");
+
+    //音频解码器初始化
+    AVCodecContext *ac=avcodec_alloc_context3(pAudioAVCodec);
+    avcodec_parameters_to_context(ac,ic->streams[audioStream]->codecpar);
+    ac->thread_count=1;
+    //打开音频解码器
+    re= avcodec_open2(ac, nullptr, nullptr);
+    if(re!=0){
+        char *err = mergeStr(2, "打开音频解码器失败:", av_err2str(re));
+        LOGE("%s",err);
+        return env->NewStringUTF(err);
+    }
+
+    LOGI("打开音频解码器成功!");
+
+    /*===========================音频解码-end============================================================*/
 
 
 
+    //读取帧数据
+    AVPacket *pPacket = av_packet_alloc();
 
+    for (;;) {
+        int re = av_read_frame(ic, pPacket);
+        if (re != 0) {
+            LOGI("读到了结尾处!");
 
+            int pos=20*r2d(ic->streams[videoStream]->time_base);
+            av_seek_frame(ic,videoStream,pos,AVSEEK_FLAG_BACKWARD|AVSEEK_FLAG_FRAME);
+            continue;
+        }
+        LOGI("stream=%d,size=%d,pts=%lld,flag=%d",pPacket->stream_index,pPacket->size,pPacket->pts,pPacket->flags);
 
-
+        av_packet_unref(pPacket);
+    }
 
 
     avformat_close_input(&ic);
